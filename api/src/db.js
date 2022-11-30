c9471797af9918185002aa7a863cebdf2beaf317
Matias Agustin Ghigi <matiasghigi@gmail.com>
2022-11-30 14:08:28 -0300

    restore

--- api/src/db.js
+++ api/src/db.js
@@ -11,7 +11,7 @@ const {
 //   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 // });
 
-const sequelize = new Sequelize(DB_DEPLOY, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
